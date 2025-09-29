040ca2a752419f13ea4b6f67529a423d2ce3cf4c
Kutman Sulaimanov <kutman.sulaimanov@alatoo.edu.kg>
2025-09-29 15:16:46 +0600

    Added src, pom.xml, mvn...

---
+++ src/test/java/com/example/applicationForm/ApplicationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
